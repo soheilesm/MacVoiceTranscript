a8c46b0a0393edbdf350f90260f4ddb595142423
soheilesm <soes@stanford.edu>
2021-07-20 22:08:08 -0700

    added install_packages bash file

---
+++ install_packages.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo "installing 'portaudio' ..."
+echo "============================="
+brew install portaudio
+
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo "installing 'elasticsearch' ..."
+echo "============================="
+brew install elasticsearch
+
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo "installing 'SpeechRecognition' ..."
+echo "============================="
+pip install SpeechRecognition
+
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo "installing 'elasticsearch' ..."
+echo "============================="
+pip install elasticsearch
+
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo "installing 'pyaudio' ..."
+echo "============================="
+pip install pyaudio
