9e3dbbd5de717f854eb36b5d3014fef472b359fe
soheilesm <soes@stanford.edu>
2021-07-20 21:38:52 -0700

    changed the message

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ recognizer = speech.Recognizer()
 mic = speech.Microphone()
 
 with mic as source:
-    print("Hey There! Please say something, and we will generate the script of what you talk about :)")
+    print("Please say something, and the script of what you talk about will get generated ...")
     recognizer.adjust_for_ambient_noise(source)
     audio = recognizer.listen(source)
     transcript = recognizer.recognize_google(audio)
