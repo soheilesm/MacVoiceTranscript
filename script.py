6262875749ba9dc893409d87f9d484e7b1a381f6
soheilesm <soes@stanford.edu>
2021-07-20 21:15:18 -0700

    added messages

--- script.py
+++ script.py
@@ -3,7 +3,8 @@ recognizer = speech.Recognizer()
 mic = speech.Microphone()
 
 with mic as source:
+    print("Hey There! Please say something, and we will generate the script of what you talk about :)")
     recognizer.adjust_for_ambient_noise(source)
     audio = recognizer.listen(source)
     transcript = recognizer.recognize_google(audio)
-    print(transcript)
+    print("The transcript of what you just sid is: ", transcript)
