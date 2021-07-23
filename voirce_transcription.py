ba5437b5d25febd4055ea1a5c84d1dc203c1063f
soheilesm <soes@stanford.edu>
2021-07-22 21:33:40 -0700

    opening applications on mac with os

--- voirce_transcription.py
+++ voirce_transcription.py
@@ -2,25 +2,37 @@ import speech_recognition as speech
 from datetime import datetime
 import os
 
-recognizer = speech.Recognizer()
-mic = speech.Microphone()
 
-with mic as source:
-    # activate the microphone
-    print("Please say something, and the script of what you talk about will get generated ...")
-    recognizer.adjust_for_ambient_noise(source)
+def buffer():
+    recognizer = speech.Recognizer()
+    mic = speech.Microphone()
 
-    # record the audio
-    audio = recognizer.listen(source)
+    with mic as source:
+        # activate the microphone
+        print("Please say something, and the script of what you talk about will get generated ...")
+        recognizer.adjust_for_ambient_noise(source)
 
-    # get the transcription
-    transcript = recognizer.recognize_google(audio)
-    print("The transcript of what you just sid is: ", transcript)
+        # record the audio
+        audio = recognizer.listen(source)
 
-    # save the audio file
-    audio_files_dir = './audio_files/'
-    audio_filename = datetime.now().strftime("%Y%m%d-%H%M%S")
-    if not os.path.exists(audio_files_dir): 
-        os.makedirs(audio_files_dir)
-    with open(audio_files_dir + "audio_" + audio_filename + ".wav", "wb") as file:
-        file.write(audio.get_wav_data())
+        # get the transcription
+        transcript = recognizer.recognize_google(audio)
+        print("The transcript of what you just sid is: ", transcript)
+
+        # save the audio file
+        audio_files_dir = './audio_files/'
+        audio_filename = datetime.now().strftime("%Y%m%d-%H%M%S")
+        if not os.path.exists(audio_files_dir): 
+            os.makedirs(audio_files_dir)
+        with open(audio_files_dir + "audio_" + audio_filename + ".wav", "wb") as file:
+            file.write(audio.get_wav_data())
+
+
+def buffer2():
+    # get a list of application installed on your Mac
+    d = '/Applications'
+    apps = list(map(lambda x: x.split('.app')[0], os.listdir(d)))
+    print(apps)
+
+    app = apps[0]
+    os.system('open ' +d+'/%s.app' %app.replace(' ','\ '))
